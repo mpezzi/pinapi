c238e70508cbdbdc85015bfe04d6965a1e269d54
Michael Pezzi <mike@thespiral.ca>
2012-08-18 11:20:27 -0400

    adjusting click event

--- pinapi_tester.js
+++ pinapi_tester.js
@@ -3,9 +3,9 @@
 Drupal.behaviors.pinapi_tester = {
   attach: function(context, settings) {
     $.each(Drupal.settings.pinapi_tester, function(selector, options){
-      $('ul.pinapi-tester-codes li', selector).css('cursor', 'pointer').click(function(){
+      $('ul.pinapi-tester-codes li', selector).bind('click', function(){
         $('#edit-code', options.form).val( $(this).data('code') );
-      });
+      }).css('cursor', 'pointer');
     });
   }
 };
